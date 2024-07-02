362af14b7f48fe7408afd6f0bb3f24f8919fd058
cccaelum <ccc.trad@gmail.com>
2024-07-02 22:06:44 +0200

    'estancada'

--- js/script.js
+++ js/script.js
@@ -10,20 +10,36 @@ const mostrarPaises = async () => {
   try {
     const countries = await fetchFlags();
     
+    countries.sort((a, b) => {
+        const nameA = a.name.common.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase(); // Convertimos el nombre común del país 'a' a mayúsculas
+        const nameB = b.name.common.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase(); // Convertimos el nombre común del país 'b' a mayúsculas
+  
+        if (nameA < nameB) {
+          return -1; // Si nameA es menor que nameB, a se coloca antes que b
+        }
+        if (nameA > nameB) {
+          return 1; // Si nameA es mayor que nameB, a se coloca después que b
+        }
+        return 0; // Si nameA y nameB son iguales, no se cambia el orden
+      });
+
     countries.forEach(country => {
       const name = country.name.common; 
       const capital = country.capital ? country.capital[0] : 'N/A';
       const population = country.population;
       const flag = country.flags[0];
+      const carSide = country.car.side;
 
       const template = `
-      <li>
+      <div class='container'>
+      <ul><li class='country'>
         <h2>${name}</h2>
         <img src="${flag}" alt="${name}" width="100">
         <p>Capital: ${capital}</p>
         <p>Población: ${population}</p>
+        <p>Lado de la carretera: ${carSide === 'left' ? 'Izquierda' : 'Derecha'}</p>
         <hr>
-      </li>
+      </li></ul></div>
     `;
     listaPaises.innerHTML += template;
     });
